b62ed2d522a7ae83042bedbc6999a2d17cfb7394
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2023-11-07 19:34:25 +0200

    Створив додаток та ввів частину коду

---
+++ LinearExpressions1/Program.cs
@@ -0,0 +1,13 @@
+﻿double a, b, c, d;
+Console.WriteLine("Enter a number (a) of type double: ");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter a number (b) of type double: ");
+b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter a number (c) of type double: ");
+c = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter a number (d) of type double: ");
+d = double.Parse(Console.ReadLine());
+double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - a * a / (b * b);
+Console.WriteLine("x = "); Console.Write(x);
+Console.Write(x);
+
